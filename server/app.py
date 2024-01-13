b66510f95aa4b993c7b9ffe125bd49a769570305
DidasMba <daniel.mbarushi@gmail.com>
2024-01-13 15:32:04 +0300

    solution

--- server/app.py
+++ server/app.py
@@ -1,3 +1,35 @@
+class NewsletterByID(Resource):
+
+    def get(self, id):
+
+        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+
+        return response
+
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
+
+Only two differences between this and our original GET route:
+
+We need to include id in our method's arguments and the resource URL.
+We need to chain some commands together to get the record with the provided id.
+Check out this lesson's finished product: open Postman and navigate to http://127.0.0.1:5555/newsletters/20Links to an external site.. Make sure that your request method is GET, then click submit. You should see something similar to the following:
+
+{
+    "body": "College tax head change. Claim exactly because choose. Church edge center across test stock.",
+    "edited_at": null,
+    "id": 20,
+    "published_at": "2022-09-21 18:35:17",
+    "title": "Court probably not."
+}
+Conclusion
+Flask-RESTful is a very simple tool that allows us to properly and effectively use HTTP request methods in our applications. Like other extensions, it reduces the amount of code you have to write to accomplish common tasks- and if you don't need to accomplish those common tasks, you can just leave it out!
+
+Solution Code
 #!/usr/bin/env python3
 
 from flask import Flask, jsonify, request, make_response
@@ -9,7 +41,7 @@ from models import db, Newsletter
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -26,28 +58,62 @@ class Index(Resource):
 
         response = make_response(
             jsonify(response_dict),
-            200
+            200,
         )
 
         return response
 
 api.add_resource(Index, '/')
 
-def post(self):
+class Newsletters(Resource):
 
-    new_record = Newsletter(
-        title=request.form['title'],
-        body=request.form['body'],
-    )
+    def get(self):
 
-    db.session.add(new_record)
-    db.session.commit()
+        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
 
-    response_dict = new_record.to_dict()
+        response = make_response(
+            jsonify(response_dict_list),
+            200,
+        )
 
-    response = make_response(
-        jsonify(response_dict),
-        201,
-    )
+        return response
 
-    return response
+    def post(self):
+
+        new_record = Newsletter(
+            title=request.form['title'],
+            body=request.form['body'],
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            201,
+        )
+
+        return response
+
+api.add_resource(Newsletters, '/newsletters')
+
+class NewsletterByID(Resource):
+
+    def get(self, id):
+
+        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+
+        return response
+
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
